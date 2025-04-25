88f6f0bbab7322676c90924015db0b3fc9b25e60
LaianeSousa <laianesilvasousa03@gmail.com>
2025-04-25 13:10:49 -0300

    Inicio da pag Pagamentos

--- ppe-front/src/App.jsx
+++ ppe-front/src/App.jsx
@@ -21,6 +21,7 @@ import GerenciarAlunos from "./pages/Admin/GerenciarAlunos/GerenciarAlunos"
 import RankingAdmin from "./pages/Common/Ranking/Ranking"
 import CursosAdmin from "./pages/Common/Cursos/Cursos"
 import CorrigirRedacoes from "./pages/Admin/CorrigirRedacoes/CorrigirRedacoes"
+import Pagamentos from "./pages/Admin/Pagamentos/pagamentos"
 
 const router = createBrowserRouter([
   {
@@ -50,6 +51,7 @@ const router = createBrowserRouter([
       { path: "ranking", element: <RankingAdmin /> },
       { path: "cursos", element: <CursosAdmin /> },
       { path: "corrigir-redacoes", element: <CorrigirRedacoes /> },
+      { path:"Pagamentos", element: <Pagamentos />}
     ],
   },
 ]);
